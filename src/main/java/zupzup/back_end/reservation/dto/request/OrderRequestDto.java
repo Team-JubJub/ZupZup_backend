b9bdd534ac44915366bfc81fa14078484de0700f
zer0who <eagle7449@naver.com>
2023-01-17 15:01:26 +0900

    upstream set test

--- src/main/java/zupzup/back_end/reservation/dto/request/OrderRequestDto.java
+++ src/main/java/zupzup/back_end/reservation/dto/request/OrderRequestDto.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class OrderRequestDto {
+public class OrderRequestDto {  //set upstream test
     //[상품 이름, 가격, 갯수]
 
     private String itemName;
