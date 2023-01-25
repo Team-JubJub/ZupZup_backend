5569e277cb8ef0b12416ff3fca9da94c02beb7c0
zer0who <eagle7449@naver.com>
2023-01-25 16:17:30 +0900

    test용 print line 삭제

--- src/main/java/zupzup/back_end/reservation/controller/OrderController.java
+++ src/main/java/zupzup/back_end/reservation/controller/OrderController.java
@@ -20,18 +20,12 @@ public class OrderController {
     public List<OrderDto.GetOrderDto> getAllOrderList() throws Exception {
         List<OrderDto.GetOrderDto> allOrderListDto = orderServiceImpl.getAllOrder();
 
-        System.out.print("In Controller ");  // for test
-        System.out.println(allOrderListDto);
-
         return allOrderListDto; // order들의 dto list 반환
     }
 
     @GetMapping("/{orderId}")  // 각 order에 대한 단건 GET
     public OrderDto.GetOrderSpecificDto getOrder(@PathVariable Long orderId) throws Exception {
         OrderDto.GetOrderSpecificDto getOrderSpecificDto = orderServiceImpl.getOrderById(orderId);
-
-        System.out.print("In Controller ");  // for test
-        System.out.println(getOrderSpecificDto);
 
         return getOrderSpecificDto; // 한 개 order의 dto 반환
     }
