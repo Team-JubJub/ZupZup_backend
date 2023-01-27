558136ef68b44acf4de9850425de7db4de055d93
zer0who <eagle7449@naver.com>
2023-01-27 20:30:27 +0900

    가게에 주문이 존재하는지에 따른 예외처리를 위한 method 생성

--- src/main/java/zupzup/back_end/reservation/service/OrderService.java
+++ src/main/java/zupzup/back_end/reservation/service/OrderService.java
@@ -5,6 +5,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import zupzup.back_end.reservation.Exception.OrderNotFoundException;
 import zupzup.back_end.reservation.domain.Order;
 import zupzup.back_end.reservation.dto.OrderDto;
 import zupzup.back_end.reservation.repository.OrderRepository;
@@ -37,6 +38,7 @@ public class OrderService {
 
     public OrderDto.GetOrderSpecificDto getOrderById(Long storeId, Long orderId) {
         Order orderEntity = orderRepository.findById(orderId).get();
+        isOrderInStore(storeId, orderEntity);
         OrderDto.GetOrderSpecificDto getOrderSpecificDto = modelMapper.map(orderEntity, OrderDto.GetOrderSpecificDto.class);
 
         return getOrderSpecificDto;
@@ -45,6 +47,7 @@ public class OrderService {
     // <-------------------- PATCH part -------------------->
     public OrderDto.GetOrderSpecificDto patchOrderById(Long storeId, Long orderId, OrderDto.PatchOrderDto patchOrderDto) {
         Order orderEntity = orderRepository.findById(orderId).get();
+        isOrderInStore(storeId, orderEntity);
         /*
             PatchOrderDto로 받아온 사장님이 입력한 예약 확정 내역(아이템 개수 등)과 orderEntity 비교해서
             Entity의 내용 수정, 수정된 내용 dto로 반환
@@ -53,5 +56,12 @@ public class OrderService {
         OrderDto.GetOrderSpecificDto patchedOrderSpecificDto = modelMapper.map(orderEntity, OrderDto.GetOrderSpecificDto.class);
 
         return patchedOrderSpecificDto;
+    }
+    // <-------------------- Common methods part -------------------->
+
+    private void isOrderInStore(Long storeId, Order orderEntity) {
+        if(orderEntity.getStore().getStoreId() != storeId){
+            throw new OrderNotFoundException();
+        }
     }
 }
