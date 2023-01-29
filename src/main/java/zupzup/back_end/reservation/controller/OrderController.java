a1eca567dd373ad732281a7878052714b8cc5dc3
zer0who <eagle7449@naver.com>
2023-01-29 21:09:10 +0900

    fix

--- src/main/java/zupzup/back_end/reservation/controller/OrderController.java
+++ src/main/java/zupzup/back_end/reservation/controller/OrderController.java
@@ -13,19 +13,19 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/{storeId}/order")
 public class OrderController {
-    private final OrderService orderServiceImpl;
+    private final OrderService orderService;
 
     // <-------------------- GET part -------------------->
     @GetMapping("")  // order에 대한 GET(주문 항목 모두)
     public List<OrderRequestDto.GetOrderDto> getAllOrderList(@PathVariable Long storeId) {
-        List<OrderRequestDto.GetOrderDto> allOrderListDto = orderServiceImpl.getAllOrder(storeId);
+        List<OrderRequestDto.GetOrderDto> allOrderListDto = orderService.getAllOrder(storeId);
 
         return allOrderListDto; // order들의 dto list 반환
     }
 
     @GetMapping("/{orderId}")  // 각 order에 대한 단건 GET
     public OrderRequestDto.GetOrderSpecificDto getOrder(@PathVariable Long storeId, @PathVariable Long orderId) {
-        OrderRequestDto.GetOrderSpecificDto getOrderSpecificDto = orderServiceImpl.getOrderById(storeId, orderId);
+        OrderRequestDto.GetOrderSpecificDto getOrderSpecificDto = orderService.getOrderById(storeId, orderId);
 
         return getOrderSpecificDto; // 한 개 order의 dto 반환
     }
@@ -33,7 +33,7 @@ public class OrderController {
     // <-------------------- PATCH part -------------------->
     @PatchMapping("/{orderId}")  // 각 order에 대해 사장님이 주문 확정시 사용할 request
     public OrderRequestDto.GetOrderSpecificDto patchOrder(@PathVariable Long storeId, @PathVariable Long orderId, @RequestBody OrderRequestDto.PatchOrderDto patchOrderDto) {
-        OrderRequestDto.GetOrderSpecificDto patchedOrderSpecificDto = orderServiceImpl.patchOrderById(storeId, orderId, patchOrderDto);
+        OrderRequestDto.GetOrderSpecificDto patchedOrderSpecificDto = orderService.patchOrderById(storeId, orderId, patchOrderDto);
 
         return patchedOrderSpecificDto; // patch 된 order의 dto 반환
     }
