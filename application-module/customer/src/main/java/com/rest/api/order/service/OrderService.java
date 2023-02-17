73f54209d400e967e00dea9f761516fabab0f0b7
zer0who <eagle7449@naver.com>
2023-02-17 19:57:02 +0900

    주문 제목, 주문한 시각 set 구현

--- application-module/customer/src/main/java/com/rest/api/order/service/OrderService.java
+++ application-module/customer/src/main/java/com/rest/api/order/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.rest.api.order.service;
 
 import domain.order.Order;
+import domain.order.type.OrderSpecific;
 import dto.order.OrderDto;
 import dto.order.customer.request.OrderRequestDto;
 import jakarta.transaction.Transactional;
@@ -22,13 +23,19 @@ public class OrderService {
 
     // <-------------------- POST part -------------------->
     public String addOrder(OrderRequestDto.PostOrderDto postOrderDto) {
-        LocalTime nowTime = LocalTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH시 mm분 ss초");
+        LocalTime nowTime = LocalTime.now();    // 주문한 시간
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");   // 09:43 am, 04:57 pm
         String formattedNowTime = nowTime.format(formatter);
+
+        OrderSpecific firstAtOrderSpecific = postOrderDto.getOrderList().get(0);    // 주문 제목에 쓰일 주문의 첫 번째 목록
+        String firstAtOrderList = firstAtOrderSpecific.getItemName();
+        int firstAtOrderListCount = firstAtOrderSpecific.getItemCount();
+        int orderListCount = postOrderDto.getOrderList().size() - 1;
 
         OrderDto orderDto = new OrderDto();
         orderDto.setUsername(postOrderDto.getUsername());
         orderDto.setPhoneNumber(postOrderDto.getPhoneNumber());
+        orderDto.setOrderTitle(firstAtOrderList + " " + firstAtOrderListCount + "개 외 " + orderListCount + "건");    // 크로플 3개 외 4건
         orderDto.setOrderTime(formattedNowTime);
         orderDto.setVisitTime(postOrderDto.getVisitTime());
         orderDto.setOrderList(postOrderDto.getOrderList());
