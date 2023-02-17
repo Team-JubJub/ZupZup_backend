2691d7c9915d308de714b500f8167298fdc24027
zer0who <eagle7449@naver.com>
2023-02-17 18:25:40 +0900

    고객님 앱 주문(post)틀 생성

--- application-module/customer/src/main/java/com/rest/api/order/controller/OrderController.java
+++ application-module/customer/src/main/java/com/rest/api/order/controller/OrderController.java
@@ -1,4 +1,27 @@
 package com.rest.api.order.controller;
 
+import com.rest.api.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log
+@RequiredArgsConstructor
+@RequestMapping("/customer/order")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("")
+    public ResponseEntity makeOrder() {
+
+
+        return new ResponseEntity("주문이 완료되었습니다.", HttpStatus.CREATED);
+    }
 }
