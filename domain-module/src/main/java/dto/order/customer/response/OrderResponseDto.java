a66722331a971a8683ca138ddf531518687a07a3
zer0who <eagle7449@naver.com>
2023-02-21 19:05:24 +0900

    GetOrderDetailsDto 정의

--- domain-module/src/main/java/dto/order/customer/response/OrderResponseDto.java
+++ domain-module/src/main/java/dto/order/customer/response/OrderResponseDto.java
@@ -1,8 +1,11 @@
 package dto.order.customer.response;
 
+import domain.order.type.OrderSpecific;
 import domain.order.type.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 public class OrderResponseDto {
 
@@ -16,6 +19,19 @@ public class OrderResponseDto {
         private String orderTitle;
         private String orderTime;
         private String visitTime;
+    }
+
+    @Getter
+    @Setter
+    public static class GetOrderDetailsDto {
+        private String storeName;   // entity에는 store 객체 저장, modelMapper가 알아서 name 빼내줌.
+        private String storeAddress; // 상동
+        private OrderStatus orderStatus;
+        private String userName; // 닉네임
+        private String phoneNumber;
+        private String orderTime;
+        private String visitTime;
+        private List<OrderSpecific> orderList;
     }
 
 }
