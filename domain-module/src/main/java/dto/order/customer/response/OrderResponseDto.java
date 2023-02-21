094452596e8ee41e021d9faa01739bd1277efaa2
zer0who <eagle7449@naver.com>
2023-02-21 17:37:33 +0900

    customer order responseDto의 order type string -> OrderStatus로 수정

--- domain-module/src/main/java/dto/order/customer/response/OrderResponseDto.java
+++ domain-module/src/main/java/dto/order/customer/response/OrderResponseDto.java
@@ -1,5 +1,6 @@
 package dto.order.customer.response;
 
+import domain.order.type.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +11,7 @@ public class OrderResponseDto {
     @Setter
     public static class GetOrderDto {
         private String storeName;   // entity에는 store 객체 저장, modelMapper가 알아서 name 빼내줌.
-        private String orderStatus;
+        private OrderStatus orderStatus;
         private String userName; // 닉네임
         private String orderTitle;
         private String orderTime;
