9c52e74c36274dc49592ff1b39269b7514849a82
zer0who <eagle7449@naver.com>
2023-03-02 17:58:57 +0900

    controller advice - constraintViolation response body 간략화

--- application-module/customer/src/main/java/com/rest/api/advice/CustomerControllerAdvice.java
+++ application-module/customer/src/main/java/com/rest/api/advice/CustomerControllerAdvice.java
@@ -3,16 +3,26 @@ package com.rest.api.advice;
 import exception.NoSuchException;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestControllerAdvice
 public class CustomerControllerAdvice {
 
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = ConstraintViolationException.class)
-    public String orderConstraintViolation(ConstraintViolationException e) { return e.getMessage(); }
+    public ResponseEntity orderConstraintViolation(ConstraintViolationException e) {
+        List<String> constraintViolations = new ArrayList<>();
+        e.getConstraintViolations().forEach(error -> {
+            String message = error.getMessage();
+            constraintViolations.add(message);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(constraintViolations);
+    }
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND)   // 가게, 주문이 존재하지 않는 경우
     @ExceptionHandler(value = NoSuchException.class)
