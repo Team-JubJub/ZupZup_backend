40b0acff7c8a85bd865ef0aa74a8c33567cc2846
seasameoil <whdwlgus12@gmail.com>
2023-03-07 21:28:32 +0900

    가게별 단건 get 완성

--- domain-module/src/main/java/dto/store/customer/response/StoreResponseDto.java
+++ domain-module/src/main/java/dto/store/customer/response/StoreResponseDto.java
@@ -1,7 +1,12 @@
 package dto.store.customer.response;
 
+import domain.item.Item;
+import dto.item.ItemDto;
+import dto.item.seller.response.ItemResponseDto;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 public class StoreResponseDto {
 
@@ -16,5 +21,32 @@ public class StoreResponseDto {
         private String saleTimeStart;
         private String saleTimeEnd;
         private String salePercent;    // "00%"
+    }
+
+    @Getter
+    @Setter
+    public static class GetStoreDetailDto {
+
+        private Long storeId;
+
+        private String storeName;
+        private String category;
+        private String storeAddress;
+
+        private String openTime;
+        private String endTime;
+        private String saleMatters;
+
+        private String saleTimeStart;
+        private String saleTimeEnd;
+
+        private String salePercent;
+
+        private Double longitude;
+        private Double latitude;
+
+        private List<String> eventList;
+
+        private List<ItemResponseDto> itemDtoList;
     }
 }
