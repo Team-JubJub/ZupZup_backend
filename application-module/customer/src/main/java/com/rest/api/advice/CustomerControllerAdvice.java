140a9441570e2cd8849d1f43b3ad75530d0ba111
zer0who <eagle7449@naver.com>
2023-03-12 16:02:53 +0900

    fix

--- application-module/customer/src/main/java/com/rest/api/advice/CustomerControllerAdvice.java
+++ application-module/customer/src/main/java/com/rest/api/advice/CustomerControllerAdvice.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
