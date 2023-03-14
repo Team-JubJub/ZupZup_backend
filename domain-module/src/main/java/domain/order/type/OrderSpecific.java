23368c99c19d2117fc7a8bd649c7f0875aafa0c5
zer0who <eagle7449@naver.com>
2023-03-14 19:23:24 +0900

    order specific item 정수 값들 int -> Integer 수정

--- domain-module/src/main/java/domain/order/type/OrderSpecific.java
+++ domain-module/src/main/java/domain/order/type/OrderSpecific.java
@@ -13,8 +13,8 @@ public class OrderSpecific {
     //[상품 이름, 가격, 갯수, (이미지)]
     private Long itemId; // 일단 단순 Long으로 받아오고, 외래키로 갈지 말지는 나중에
     private String itemName;    // 상품 이름
-    private int itemPrice;  // 가격
+    private Integer itemPrice;  // 가격
     @Min(value = 0, message = "상품의 개수는 0개 미만일 수 없습니다.")
-    private int itemCount;  // 갯수
+    private Integer itemCount;  // 갯수
 
 }
