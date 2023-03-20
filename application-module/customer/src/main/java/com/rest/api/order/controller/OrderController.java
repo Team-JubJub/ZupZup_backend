49c875e5b0589dbc27b1ac6f9536996a64e321b0
zer0who <eagle7449@naver.com>
2023-03-20 16:28:38 +0900

    fix

--- application-module/customer/src/main/java/com/rest/api/order/controller/OrderController.java
+++ application-module/customer/src/main/java/com/rest/api/order/controller/OrderController.java
@@ -23,7 +23,7 @@ public class OrderController {
     private final OrderService orderService;
 
     // <-------------------- POST part -------------------->
-    @PostMapping("/store/{storeId}/order")    // end point에 order 추가돼야할지 고민할 것
+    @PostMapping("/store/{storeId}/order")
     public ResponseEntity addOrder(@PathVariable Long storeId, @RequestBody @Valid OrderRequestDto.PostOrderDto postOrderDto) {
         String addOrderResult = orderService.addOrder(storeId, postOrderDto);
 
