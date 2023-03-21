f847c131a3c19b9559a12882948d516aa203c823
zer0who <eagle7449@naver.com>
2023-03-21 16:05:04 +0900

    order - POST - response dto 정의

--- domain-module/src/main/java/dto/order/customer/response/OrderResponseDto.java
+++ domain-module/src/main/java/dto/order/customer/response/OrderResponseDto.java
@@ -8,6 +8,21 @@ import lombok.Setter;
 import java.util.List;
 
 public class OrderResponseDto {
+    // <-------------------- POST part -------------------->
+    @Getter
+    @Setter
+    public static class PostOrderDto {
+        private String storeName;   // entity에는 store 객체 저장, modelMapper가 알아서 name 빼내줌.
+        private String storeAddress; // 상동
+        private OrderStatus orderStatus;
+        private String userName; // 닉네임
+        private String phoneNumber;
+        private String orderTime;
+        private String visitTime;
+        private List<OrderSpecific> orderList;
+        private String message;
+        private String href;
+    }
 
     // <-------------------- GET part -------------------->
     @Getter
