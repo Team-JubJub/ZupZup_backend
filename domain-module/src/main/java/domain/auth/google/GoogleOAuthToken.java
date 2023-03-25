b8a1ffba309cbf8927f4b2f32cb343285e1317c1
zer0who <eagle7449@naver.com>
2023-03-25 15:45:25 +0900

    Google User 패키지 내부 클래스, 소셜 로그인 공통 result 클래스 정의

---
+++ domain-module/src/main/java/domain/auth/google/GoogleOAuthToken.java
@@ -0,0 +1,16 @@
+package domain.auth.google;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class GoogleOAuthToken { // 구글에 일회성 코드를 다시 보내 받아올 액세스 토큰을 포함한 JSON 문자열을 담을 클래스
+
+    private String access_token;
+    private int expires_in;
+    private String scope;
+    private String token_type;
+    private String id_token;
+
+}
