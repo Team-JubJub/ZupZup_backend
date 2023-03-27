2290910138e89c72cb7a6541254f43789108d18c
zer0who <eagle7449@naver.com>
2023-03-27 16:33:49 +0900

    UserDto 정의 및 CustomOAuth2Service에 적용

--- application-module/customer/src/main/java/com/rest/api/auth/service/CustomOAuth2UserService.java
+++ application-module/customer/src/main/java/com/rest/api/auth/service/CustomOAuth2UserService.java
@@ -1,7 +1,9 @@
 package com.rest.api.auth.service;
 
 import domain.auth.Provider;
+import domain.auth.Role;
 import domain.auth.User;
+import dto.auth.customer.UserDto;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -30,16 +32,20 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2UserService delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
+        String email;
+        User userEntity;
+        UserDto userDto = new UserDto();
 
         // 서비스 구분을 위한 작업(구글 : google, 네이버 : naver, ...)
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
 
-        String email;
         Map<String, Object> response = oAuth2User.getAttributes();
 
         if(registrationId.equals(Provider.GOOGLE)) {   // In case of Google Login
             email = (String)response.get("email");
+            userDto.setProvider(Provider.GOOGLE);
+            userDto.setEmail(email);
         }
 //        else if(registrationId.equals("naver")) {   // In case of Naver Login
 //            Map<String, Object> hash = (Map<String, Object>)response.get("response");
@@ -49,14 +55,15 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
             throw new OAuth2AuthenticationException("아직 구현되지 않은 인증입니다.");
         }
 
-        User userEntity;
-        Optional<User> optionalUserEntity = userRepository.findByEmail(email);
+        Optional<User> optionalUserEntity = userRepository.findByEmail(email);  // User가 DB에 있는지 없는지 여부 확인 용
 
-        if(optionalUserEntity.isPresent()) {  // 이미 가입한(ZupZup에) 사람에 대한 로직
+        if(optionalUserEntity.isPresent()) {  // 이미 가입한(ZupZup에) user에 대한 로직
             userEntity = optionalUserEntity.get();
         }
-        else {  // 가입하지 않은 유저 -> DB에 저장
-            userEntity = User.builder(email)
+        else {  // 가입하지 않은 user -> DB에 저장
+            userEntity = User.builder(userDto.getEmail())
+                    .role(Role.ROLE_USER)
+                    .provider(userDto.getProvider())
                     .build();
             userRepository.save(userEntity);
         }
--- domain-module/src/main/java/dto/auth/customer/UserDto.java
+++ domain-module/src/main/java/dto/auth/customer/UserDto.java
@@ -2,10 +2,15 @@ package dto.auth.customer;
 
 import domain.auth.Provider;
 import domain.auth.Role;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UserDto {
 
     private Long id;
+
     private String email;
     private String password;
     private Role role;
