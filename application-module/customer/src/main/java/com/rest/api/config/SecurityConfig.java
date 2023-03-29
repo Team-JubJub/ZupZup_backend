b14c96bd7afb76614d3ecbe8f4a75b09bb0bbbbb
zer0who <eagle7449@naver.com>
2023-03-30 01:01:13 +0900

    Success handler 구현, Security Config 이때까지 구현 내용에 맞추어 수정

--- application-module/customer/src/main/java/com/rest/api/config/SecurityConfig.java
+++ application-module/customer/src/main/java/com/rest/api/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.rest.api.config;
 
 import com.rest.api.auth.handler.OAuth2AuthenticationFailureHandler;
 import com.rest.api.auth.handler.OAuth2AuthenticationSuccessHandler;
+import com.rest.api.auth.jwt.JwtAuthenticationFilter;
+import com.rest.api.auth.jwt.JwtTokenProvider;
+import com.rest.api.auth.repository.HttpCookieOAuth2AuthorizationRequestRepository;
 import com.rest.api.auth.service.CustomOAuth2UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -16,14 +19,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
     private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
     private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler, OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler) {
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider, CustomOAuth2UserService customOAuth2UserService, OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler, OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler) {
+        this.jwtTokenProvider = jwtTokenProvider;
         this.customOAuth2UserService = customOAuth2UserService;
         this.oAuth2AuthenticationSuccessHandler = oAuth2AuthenticationSuccessHandler;
         this.oAuth2AuthenticationFailureHandler = oAuth2AuthenticationFailureHandler;
+    }
+
+    @Bean
+    public HttpCookieOAuth2AuthorizationRequestRepository cookieOAuth2AuthorizationRequestRepository() {
+        return new HttpCookieOAuth2AuthorizationRequestRepository();
     }
 
     @Bean
@@ -44,9 +54,14 @@ public class SecurityConfig {
 //                .logout()
 //                .logoutSuccessUrl("/")
                 .and()
-                    .oauth2Login().loginPage("/login/oauth2")    // 인증 필요한 URL 접근 시 이동할 login page
-                    .defaultSuccessUrl("/") // 로그인 성공 시 이동할 곳
-                    .failureUrl("/login")   // 로그인 실패 시 이동할 곳
+                    .oauth2Login()
+                    .authorizationEndpoint().baseUri("/login/oauth2")
+                    .authorizationRequestRepository(cookieOAuth2AuthorizationRequestRepository())
+                .and()
+                    .redirectionEndpoint()
+                    .baseUri("/login/oauth2/code/**")   // 일단 이렇게 두고 나중에 수정
+//                    .loginPage("/login/oauth2")    // 인증 필요한 URL 접근 시 이동할 login page
+                .and()
                     .userInfoEndpoint().userService(customOAuth2UserService)   // // 로그인 성공 후 사용자 정보를 가져옴, 사용자 정보 처리 시 사용 될 service
                 .and()
                     .successHandler(oAuth2AuthenticationSuccessHandler)
