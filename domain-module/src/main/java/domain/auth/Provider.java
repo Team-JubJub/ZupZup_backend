b12eb8cbcd0f0b6e0e6b00290280bed8afe864b2
김영후 <eagle7449@naver.com>
2023-04-04 17:11:34 +0900

    provider에 생성자, getter, 값 추가

--- domain-module/src/main/java/domain/auth/Provider.java
+++ domain-module/src/main/java/domain/auth/Provider.java
@@ -2,9 +2,15 @@ package domain.auth;
 
 public enum Provider {
 
-    GOOGLE,
-    NAVER,
-    KAKAO,
-    APPLE;
+    GOOGLE("구글"),
+    NAVER("네이버"),
+    KAKAO("카카오"),
+    APPLE("애플");
+
+    private final String provider;
+
+    Provider(String provider) { this.provider = provider; }
+
+    public String getProvider() { return provider; }
     
 }
