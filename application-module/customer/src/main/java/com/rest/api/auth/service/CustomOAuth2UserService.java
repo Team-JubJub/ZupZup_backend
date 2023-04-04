1e2cc85d4506b4b7de2a19c31497dfa203235e8a
김영후 <eagle7449@naver.com>
2023-04-05 01:06:17 +0900

    temp

--- application-module/customer/src/main/java/com/rest/api/auth/service/CustomOAuth2UserService.java
+++ application-module/customer/src/main/java/com/rest/api/auth/service/CustomOAuth2UserService.java
@@ -32,7 +32,9 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2UserService delegate = new DefaultOAuth2UserService();    // 기본 처리 Service를 내가 정의한 Service에 위임함.
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
-        String email;
+//        String email;
+        String phoneNumber; // 폰 넘버나
+        String userName;    // 이름 기반 최초로그인 판단해야할 것 같음. -> 의논해볼 것
         User userEntity;
         UserDto userDto = new UserDto();
 
@@ -42,21 +44,23 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
         Map<String, Object> response = oAuth2User.getAttributes();
 
-        if(registrationId.equals(Provider.GOOGLE)) {   // In case of Google Login
-            email = (String)response.get("email");
-            userDto.setProvider(Provider.GOOGLE);
-            userDto.setEmail(email);
-        }
-//        else if(registrationId.equals("naver")) {   // In case of Naver Login
-//            Map<String, Object> hash = (Map<String, Object>)response.get("response");
-//            email = (String)hash.get("email");
+//        if(registrationId.equals(Provider.GOOGLE)) {   // In case of Google Login
+//            email = (String)response.get("email");
+//            userDto.setProvider(Provider.GOOGLE);
+//            userDto.setEmail(email);
 //        }
+        if(registrationId.equals(Provider.NAVER)) {   // In case of Naver Login
+            Map<String, Object> hash = (Map<String, Object>)response.get("response");
+            userDto.setProvider(Provider.NAVER);
+            userDto.setRole(Role.ROLE_USER);
+            phoneNumber = "temp";
+        }
         else {
-            throw new OAuth2AuthenticationException("아직 구현되지 않은 인증입니다.");
+            throw new OAuth2AuthenticationException("구현되지 않은 인증입니다.");
         }
 
-        Optional<User> optionalUserEntity = userRepository.findByEmail(email);  // User가 DB에 있는지 없는지 여부 확인 용
-
+        Optional<User> optionalUserEntity = userRepository.findByPhoneNumber(phoneNumber);  // User가 DB에 있는지 없는지 여부 확인 용
+        // 생각해보니 최초로그인인지 판단하려면 로그인 api에서 정보 받아와서 db에 있는지 확인해야함.
         if(optionalUserEntity.isPresent()) {  // 이미 가입한(ZupZup에) user에 대한 로직
             userEntity = optionalUserEntity.get();
         }
