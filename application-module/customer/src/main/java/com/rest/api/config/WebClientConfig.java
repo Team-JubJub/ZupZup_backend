ef5b1034c30a54ee56c183c7f565c618fffb33af
김영후 <eagle7449@naver.com>
2023-04-06 19:52:47 +0900

    WebClientConfig 등록

---
+++ application-module/customer/src/main/java/com/rest/api/config/WebClientConfig.java
@@ -0,0 +1,14 @@
+package com.rest.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.create();  // REST 통신을 위한 API 등록
+    }
+}
