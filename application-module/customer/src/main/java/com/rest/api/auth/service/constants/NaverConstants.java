24f4cc2c3bf58628f7cdec8f890c3e1d7e88a051
김영후 <eagle7449@naver.com>
2023-04-07 16:11:22 +0900

    add authorization_uri

--- application-module/customer/src/main/java/com/rest/api/auth/service/constants/NaverConstants.java
+++ application-module/customer/src/main/java/com/rest/api/auth/service/constants/NaverConstants.java
@@ -10,9 +10,11 @@ public class NaverConstants {
     private String client_id;
     @Value("${spring.security.oauth2.client.registration.naver.client_secret}")
     private String client_secret;
-    @Value("${spring.security.oauth2.client.registration.provider.naver.token_uri}")
+    @Value("${spring.security.oauth2.client.provider.naver.authorization_uri}")
+    private String authorization_uri;
+    @Value("${spring.security.oauth2.client.provider.naver.token_uri}")
     private String token_uri;
-    @Value("${spring.security.oauth2.client.registration.provider.naver.user_info_uri}")
+    @Value("${spring.security.oauth2.client.provider.naver.user_info_uri}")
     private String user_info_uri;
 
 }
