93a591f82637d4fb646ed81789110c4110d1aca6
김영후 <eagle7449@naver.com>
2023-04-11 16:32:36 +0900

    MobileOAuthController에 requestMapper추가(/mobile)

--- application-module/customer/src/main/java/com/rest/api/auth/controller/MobileOAuthController.java
+++ application-module/customer/src/main/java/com/rest/api/auth/controller/MobileOAuthController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/mobile")
 @RequiredArgsConstructor
 public class MobileOAuthController {
     /*
