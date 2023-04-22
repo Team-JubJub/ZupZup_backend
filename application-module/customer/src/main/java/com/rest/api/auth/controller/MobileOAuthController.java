60b11834dfbff25cc16574695e00bfb4b69a507f
김영후 <eagle7449@naver.com>
2023-04-22 20:43:35 +0900

    재로그인 토큰 전달 쿠키 -> 헤더로 변경

--- application-module/customer/src/main/java/com/rest/api/auth/controller/MobileOAuthController.java
+++ application-module/customer/src/main/java/com/rest/api/auth/controller/MobileOAuthController.java
@@ -48,7 +48,7 @@ public class MobileOAuthController {
     @PostMapping("/sign-up/{provider}")    // 회원가입 요청
     public ResponseEntity signUp(@Parameter(name = "provider", description = "소셜 플랫폼 종류(소문자)", in = ParameterIn.PATH,
             content = @Content(schema = @Schema(type = "string", allowableValues = {"naver", "kakao", "google", "apple"}))) @PathVariable String provider,
-                                 @RequestBody UserRequestDto.UserSignUpDto userSignUpDto, HttpServletResponse response) {   // ex) ~/sign-in/naver?access_token=...&refresh_token=... + body: { userUniqueId: "naver에서 준 ID" }
+                                 @RequestBody UserRequestDto.UserSignUpDto userSignUpDto) {   // ex) ~/sign-in/naver?access_token=...&refresh_token=... + body: { userUniqueId: "naver에서 준 ID" }
         TokenInfoDto signUpResult = mobileOAuthService.signUp(provider, userSignUpDto); // service layer에서 user 정보 저장, refresh token redis에 저장까지
 //        Cookie accessTokenCookie = new Cookie(JwtTokenProvider.ACCESS_TOKEN_NAME, signUpResult.getAccessToken());   // 쿠키 set
 //        Cookie refreshTokenCookie = new Cookie(JwtTokenProvider.REFRESH_TOKEN_NAME, signUpResult.getRefreshToken());
@@ -97,16 +97,19 @@ public class MobileOAuthController {
     @PostMapping("/sign-in/{provider}")  // 로그인 요청(access, refresh token 모두 만료일 경우)
     public ResponseEntity signInWithProviderUserId(@Parameter(name = "provider", description = "소셜 플랫폼 종류(소문자)", in = ParameterIn.PATH,
             content = @Content(schema = @Schema(type = "string", allowableValues = {"naver", "kakao", "google", "apple"}))) @PathVariable String provider,
-            @RequestBody UserRequestDto.UserSignInDto userSignInDto, HttpServletResponse response) {
+            @RequestBody UserRequestDto.UserSignInDto userSignInDto) {
         TokenInfoDto reSignInResult = mobileOAuthService.signInWithProviderUserId(provider, userSignInDto);
-        Cookie accessTokenCookie = new Cookie(JwtTokenProvider.ACCESS_TOKEN_NAME, reSignInResult.getAccessToken());   // 쿠키 set
-        Cookie refreshTokenCookie = new Cookie(JwtTokenProvider.REFRESH_TOKEN_NAME, reSignInResult.getRefreshToken());
-        accessTokenCookie.setMaxAge((int) (JwtTokenProvider.ACCESS_TOKEN_VALIDITY_IN_MILLISECONDS / 1000));
-        refreshTokenCookie.setMaxAge((int) (JwtTokenProvider.REFRESH_TOKEN_VALIDITY_IN_MILLISECONDS / 1000));
-        response.addCookie(accessTokenCookie);
-        response.addCookie(refreshTokenCookie);
+//        Cookie accessTokenCookie = new Cookie(JwtTokenProvider.ACCESS_TOKEN_NAME, reSignInResult.getAccessToken());   // 쿠키 set
+//        Cookie refreshTokenCookie = new Cookie(JwtTokenProvider.REFRESH_TOKEN_NAME, reSignInResult.getRefreshToken());
+//        accessTokenCookie.setMaxAge((int) (JwtTokenProvider.ACCESS_TOKEN_VALIDITY_IN_MILLISECONDS / 1000));
+//        refreshTokenCookie.setMaxAge((int) (JwtTokenProvider.REFRESH_TOKEN_VALIDITY_IN_MILLISECONDS / 1000));
+//        response.addCookie(accessTokenCookie);
+//        response.addCookie(refreshTokenCookie);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set(JwtTokenProvider.ACCESS_TOKEN_NAME, reSignInResult.getAccessToken());
+        responseHeaders.set(JwtTokenProvider.REFRESH_TOKEN_NAME, reSignInResult.getRefreshToken());
 
-        return new ResponseEntity(reSignInResult, HttpStatus.OK);
+        return new ResponseEntity(reSignInResult, responseHeaders, HttpStatus.OK);
     }
 
     @Operation(summary = "로그아웃", description = "로그아웃 요청")
