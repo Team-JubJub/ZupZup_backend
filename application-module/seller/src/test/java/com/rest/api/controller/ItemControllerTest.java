303728d5058cf5a2e73eb0085b173dea7fe5237c
김영후 <eagle7449@naver.com>
2023-04-28 23:18:18 +0900

    fix

--- application-module/seller/src/test/java/com/rest/api/controller/ItemControllerTest.java
+++ application-module/seller/src/test/java/com/rest/api/controller/ItemControllerTest.java
@@ -1,24 +1,24 @@
-package com.rest.api.controller;
-
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-import repository.ItemRepository;
-import repository.StoreRepository;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Transactional
-public class ItemControllerTest {
-
-    @Autowired private ItemRepository itemRepository;
-    @Autowired private StoreRepository storeRepository;
-
-    @Test
-    public void 상품저장() {
-
-    }
-}
+//package com.rest.api.controller;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.transaction.annotation.Transactional;
+//import repository.ItemRepository;
+//import repository.StoreRepository;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@Transactional
+//public class ItemControllerTest {
+//
+//    @Autowired private ItemRepository itemRepository;
+//    @Autowired private StoreRepository storeRepository;
+//
+//    @Test
+//    public void 상품저장() {
+//
+//    }
+//}
