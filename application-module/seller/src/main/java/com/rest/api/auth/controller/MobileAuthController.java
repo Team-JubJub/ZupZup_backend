e5f0f92cdbadb50fa5aa357bf540a37e06f7a694
김영후 <eagle7449@naver.com>
2023-05-06 19:56:33 +0900

    사장님 앱 로그인 url 매핑

--- application-module/seller/src/main/java/com/rest/api/auth/controller/MobileAuthController.java
+++ application-module/seller/src/main/java/com/rest/api/auth/controller/MobileAuthController.java
@@ -1,4 +1,14 @@
 package com.rest.api.auth.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log
+@RequiredArgsConstructor
+@RequestMapping("/mobile")
 public class MobileAuthController {
+
 }
