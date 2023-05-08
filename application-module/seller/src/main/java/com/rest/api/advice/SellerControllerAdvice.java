360705c8d8218ffec0aee281674486547d3a923b
김영후 <eagle7449@naver.com>
2023-05-08 18:41:39 +0900

    예외 핸들러 함수명 변경

--- application-module/seller/src/main/java/com/rest/api/advice/SellerControllerAdvice.java
+++ application-module/seller/src/main/java/com/rest/api/advice/SellerControllerAdvice.java
@@ -40,7 +40,7 @@ public class SellerControllerAdvice {
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND)   // 가게, 주문이 존재하지 않는 경우
     @ExceptionHandler(value = NoSuchException.class)
-    public String reservationNoSuch(NoSuchException e) {
+    public String sellerEntireNoSuch(NoSuchException e) {
         return e.getMessage();
     }   // 후에 수정(이름 등) 필요할 듯
 
