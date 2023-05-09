e3211f9808716d2da75284227d4d6f24e8fb7d1d
김영후 <eagle7449@naver.com>
2023-05-09 17:00:22 +0900

    로그인 명세 작성

--- application-module/seller/src/main/java/com/rest/api/auth/controller/MobileAuthController.java
+++ application-module/seller/src/main/java/com/rest/api/auth/controller/MobileAuthController.java
@@ -4,6 +4,13 @@ import com.rest.api.auth.service.MobileAuthService;
 import domain.store.Store;
 import dto.auth.seller.request.AuthRequestDto;
 import dto.auth.seller.response.AuthResponseDto;
+import dto.auth.token.RefreshResultDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
@@ -19,6 +26,15 @@ public class MobileAuthController {
     private final MobileAuthService mobileAuthService;
 
     // < -------------- Sign-in part -------------- >
+    @Operation(summary = "로그인", description = "로그인 성공 시 firebase에 저장된 가게의 키값을 리턴해줌.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공",
+                    content = @Content(schema = @Schema(implementation = AuthRequestDto.SellerSignInDto.class))),
+            @ApiResponse(responseCode = "400", description = "비밀번호가 다름",
+                    content = @Content(schema = @Schema(implementation = AuthRequestDto.SellerSignInDto.class))),
+            @ApiResponse(responseCode = "404", description = "찾는 유저가 없는 경우",
+                    content = @Content(schema = @Schema(example = "No user found")))
+    })
     @PostMapping("/sign-in")
     public ResponseEntity singIn(@RequestBody AuthRequestDto.SellerSignInDto sellerSignInDto) {
         AuthResponseDto.SignInResponseDto signInResponseDto = mobileAuthService.signIn(sellerSignInDto);
