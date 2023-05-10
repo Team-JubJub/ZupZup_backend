8611e94b7558ff5e962289e9e1ae7b833dd5b132
김영후 <eagle7449@naver.com>
2023-05-10 20:13:40 +0900

    fix

--- domain-module/src/main/java/dto/auth/customer/request/UserRequestDto.java
+++ domain-module/src/main/java/dto/auth/customer/request/UserRequestDto.java
@@ -54,7 +54,7 @@ public class UserRequestDto {
     public static class AccountRecoveryDto {
         @Schema(description = "계정을 찾고자 하는 유저의 전화번호", example = "010-2828-7449")
         @NotBlank(message = "PhoneNumber cannot be null or empty or space")
-        @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "Phone number pattern should be like \"010-xxxx-xxxx\"")
+        @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "Phone number pattern should be like 010-xxxx-xxxx")
         private String phoneNumber;    // 클라이언트에서 제공한 소셜 플랫폼의 user unique ID
     }
 
