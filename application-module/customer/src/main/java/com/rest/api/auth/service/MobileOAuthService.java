8c47b2f2856dde604ac7df5cca870dc4d28ab83a
김영후 <eagle7449@naver.com>
2023-06-17 18:54:55 +0900

    fix

--- application-module/customer/src/main/java/com/rest/api/auth/service/MobileOAuthService.java
+++ application-module/customer/src/main/java/com/rest/api/auth/service/MobileOAuthService.java
@@ -21,8 +21,6 @@ import org.springframework.stereotype.Service;
 import repository.UserRepository;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
