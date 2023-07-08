c279fc8cb690081720e86d3ca3c99cc3eb3afc1a
김영후 <eagle7449@naver.com>
2023-07-08 15:39:12 +0900

    user entity에 orderCount 추가

--- domain-module/src/main/java/domain/auth/User/User.java
+++ domain-module/src/main/java/domain/auth/User/User.java
@@ -14,6 +14,8 @@ public class User {
     @Column(name = "userId")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;    // auto increment id
+    @Enumerated(EnumType.STRING)
+    private Provider provider;  // 가입 시 사용한 플랫폼
     @Column(nullable = false)
     private String providerUserId;  // ex) KAKAO_user123
     @Column(nullable = false)
@@ -26,12 +28,11 @@ public class User {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role;  // 유저 권한
-    @Enumerated(EnumType.STRING)
-    private Provider provider;  // 가입 시 사용한 플랫폼
 
     @Column(nullable = false) private Boolean essentialTerms;   // 필수 약관 동의 여부
     @Column(nullable = false) private Boolean optionalTerm1;    // 선택 약관1 동의 여부
     @Column(nullable = false) private String registerTime;  // 가입 시간(LocalDateTime, 현재는 KST 기준)
+    @Column(nullable = false) private int orderCount;   // 주문 횟수(바로바로 횟수 계산이 가능하게끔 primitive type으로)
 
     public static UserBuilder builder(String providerUserId) {  // 현재 필수 파라미터는 임시
         if(providerUserId.equals(null)) {
