8e5ddd0262a8f5fc8def2773476d1f4af89bc8c8
seasameoil <whdwlgus12@gmail.com>
2023-07-08 16:28:33 +0900

    엔티티 수정

--- domain-module/src/main/java/domain/store/Store.java
+++ domain-module/src/main/java/domain/store/Store.java
@@ -26,44 +26,35 @@ public class Store {
     @Column(name = "storeId")
     @Getter
     private Long storeId;
-    private Long fireBaseStoreId;
 
-    private String loginId; // 분리할 예정. 의논 후 여부 결정.
-    private String loginPwd;    // 상동
+    @JoinColumn(name = "seller")
+    private Long sellerId;
 
     @Column(nullable = false)
-    private String storeName;   //가게이름
-    @Column(nullable = false)
-    private String category;    // 가게 카테고리
+    private String storeName; // 가게이름
+    private String storeImageUrl; // 가게 대표 이미지 url - 이미지 없을 시 기본이미지
     @Column(nullable = false, length = 1000)
-    private String storeAddress;    //가게 주소
+    private String storeAddress; // 가게 주소
     @Column(nullable = false)
-    private String openTime;    // 시작 시간
+    private String category; // 카테고리
     @Column(nullable = false)
-    private String endTime; // 마감 시간
-    @Column
-    private String saleMatters; // 공지사항?
-    @Column(nullable = false)
-    private String saleTimeStart;   // 할인 시작 시간
-    @Column(nullable = false)
-    private String saleTimeEnd; // 할인 마감 시간
-    @Column // nullable true
-    private String salePercent; // 할인률? 이건 삭제될 듯.
+    private String contact; // 카테고리
     @Column(nullable = false)
     private Double longitude;   // 경도
     @Column(nullable = false)
     private Double latitude;    // 위도
-    @Convert(converter = StringListConverter.class)
-    @Column private List<String> eventList; // 이벤트 리스트? 스트링? 뭐가 될지 의논해볼 것.
-
-    @OneToMany(
-            mappedBy = "store", // item 객체의 store와 연결
-            //cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
-            orphanRemoval = true,
-            fetch = FetchType.EAGER
-    )
-    @JsonIgnore
-    private List<Item> storeItems = new ArrayList<>();
+    @Column(nullable = false)
+    private String openTime; // 운영 시작 시간
+    @Column(nullable = false)
+    private String closeTime; // 운영 마감 시간
+    @Column(nullable = false)
+    private String saleTimeStart;   // 할인 시작 시간
+    @Column(nullable = false)
+    private String saleTimeEnd; // 할인 마감 시간
+    @Column
+    private String saleMatters; // 공지사항
+    @Column
+    private Boolean isOpen; // 가게 운영 여부
 
     public static StoreBuilder builder(String storeName) {   // 필수 파라미터 고려해볼 것
         if(storeName == null) {
@@ -77,14 +68,11 @@ public class Store {
         this.storeName = storeDto.getStoreName();
         this.storeAddress = storeDto.getStoreAddress();
         this.openTime = storeDto.getOpenTime();
-        this.endTime = storeDto.getEndTime();
         this.saleMatters = storeDto.getSaleMatters();
         this.saleTimeStart = storeDto.getSaleTimeStart();
         this.saleTimeEnd = storeDto.getSaleTimeEnd();
         this.longitude = storeDto.getLongitude();
         this.latitude = storeDto.getLatitude();
-        this.eventList = storeDto.getEventList();
-        this.storeItems = storeDto.getStoreItems();
     }
 
 }
