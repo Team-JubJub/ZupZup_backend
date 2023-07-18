c19c8d9b73f0b563e2112f4eede130b211a6b50e
김영후 <eagle7449@naver.com>
2023-07-18 23:46:47 +0900

    fix

--- application-module/seller/src/main/java/com/rest/api/config/SecurityConfig.java
+++ application-module/seller/src/main/java/com/rest/api/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
