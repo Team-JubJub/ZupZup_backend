04cc45793c6e9a35650cd6f6fccbacd448781266
김영후 <eagle7449@naver.com>
2023-07-20 23:29:40 +0900

    security config 수정

--- application-module/seller/src/main/java/com/rest/api/config/SecurityConfig.java
+++ application-module/seller/src/main/java/com/rest/api/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.rest.api.config;
 
+import com.rest.api.auth.jwt.JwtAuthenticationFilter;
+import com.rest.api.auth.jwt.JwtExceptionFilter;
+import com.rest.api.auth.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -18,14 +22,16 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer(){
         return web -> {
             web.ignoring()
                     .requestMatchers( "", "/", "/error/**", "http://localhost:8082/**", "/swagger-ui/**", "/v3/api-docs/**") // H2, swagger permit all
+                    .requestMatchers("/mobile/sign-in/**")  // 로그인 관련
 //                    .requestMatchers("/seller/**")
                     .requestMatchers("/seller/test/sign-in")    // test login 헤더 없이도 되게
-                    .requestMatchers("/mobile/sign-in/**")
                     .requestMatchers("/mobile/test/sign-up");
         };
     }
@@ -33,31 +39,34 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {    // Http Security part
         http
-                .cors(c -> {
-                    CorsConfigurationSource source = request -> {
-                        // Cors 허용 패턴
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(
-                                List.of("*")
-                        );
-                        config.setAllowedMethods(
-                                List.of("*")
-                        );
-                        return config;
-                    };
-                    c.configurationSource(source);
-                })
-                .csrf().disable()   // 쿠키를 통한 보안 인증을 하지 않으면 disable 처리해도 좋다고 함. 토큰 전달 방식 쿠키 -> 헤더로 변경할 것.
-                .httpBasic().disable()  // http basic Auth 기반 인증 창(httpBasic) 안뜨게(disable)
-                .headers().frameOptions().disable() // iFrame과 관련한 보안 설정을 disable(현재는 H2 콘솔의 사용을 위해 disable처리)
+                    .cors(c -> {
+                        CorsConfigurationSource source = request -> {
+                            // Cors 허용 패턴
+                            CorsConfiguration config = new CorsConfiguration();
+                            config.setAllowedOrigins(
+                                    List.of("*")
+                            );
+                            config.setAllowedMethods(
+                                    List.of("*")
+                            );
+                            return config;
+                        };
+                        c.configurationSource(source);
+                    })
+                    .csrf().disable()   // 쿠키를 통한 보안 인증을 하지 않으면 disable 처리해도 좋다고 함. 토큰 전달 방식 쿠키 -> 헤더로 변경할 것.
+                    .httpBasic().disable()  // http basic Auth 기반 인증 창(httpBasic) 안뜨게(disable)
+                    .headers().frameOptions().disable() // iFrame과 관련한 보안 설정을 disable(현재는 H2 콘솔의 사용을 위해 disable처리)
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeHttpRequests()    // authorizeRequests() -> authorizeHttpRequests()
-                .requestMatchers( "/", "http://localhost:8082/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll() // H2, swagger permit all
-                .requestMatchers("/seller/test/sign-in").permitAll()    // test login 헤더 없이도 되게
-//                .requestMatchers("/seller/**").permitAll() // 원래 있던 파트 로그인 없이 테스트할 수 있게 임시 처리)
-                .requestMatchers("/mobile/sign-in/**").permitAll()    // 회원가입, 로그인, 계정 찾기 permit all
-                .anyRequest().authenticated();   // permitAll() 이외의 모든 request authenticated 처리
+                    .requestMatchers( "/", "http://localhost:8082/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll() // H2, swagger permit all
+                    .requestMatchers("/mobile/sign-in/**").permitAll()    // 회원가입, 로그인, 계정 찾기 permit all
+                    .requestMatchers("/seller/test/sign-in").permitAll()    // test login 헤더 없이도 되게
+//                .requestMatchers("/seller/**").permitAll() // 원래 있던 파트 로그인 없이 테스트할 수 있게 임시 처리
+                    .anyRequest().authenticated()   // permitAll() 이외의 모든 request authenticated 처리
+                .and()  // Filter로 JwtAuthenticationFilter 적용, 그 앞에 Exception handle 위해 JwtExceptionFilter 추가
+                    .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                    .addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class);
 
         return http.build();
     }
