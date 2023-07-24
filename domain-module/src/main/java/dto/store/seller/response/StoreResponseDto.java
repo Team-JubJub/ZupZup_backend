25228efa86e1ed6c68d19d5bc45ff8f18e640017
김영후 <eagle7449@naver.com>
2023-07-24 22:06:05 +0900

    store 단건 GET 완성

--- domain-module/src/main/java/dto/store/seller/response/StoreResponseDto.java
+++ domain-module/src/main/java/dto/store/seller/response/StoreResponseDto.java
@@ -5,6 +5,27 @@ import lombok.Setter;
 
 public class StoreResponseDto {
 
+    @Getter
+    @Setter
+    public static class GetStoreDetailsDto {
+        private Long storeId;
+        private Long sellerId;
+        private String storeName; //가게이름
+        private String storeImageUrl;
+        private String storeAddress;
+        private String category;
+        private String contact;
+        private Double longitude;
+        private Double latitude;
+        private String openTime;
+        private String closeTime;
+        private String saleTimeStart;
+        private String saleTimeEnd;
+        private String saleMatters; //영업 관련 사항 / ex. 공휴일 휴무
+        private Boolean isOpen;
+        private String closedDay;
+    }
+
     @Getter @Setter
     public static class response {
 
