98e8e8c8c000a4b2c71d2fe46eba723ef9c3f794
김영후 <eagle7449@naver.com>
2023-08-05 22:32:37 +0900

    dto 설명 추가

--- domain-module/src/main/java/dto/info/customer/request/PatchNickNameDto.java
+++ domain-module/src/main/java/dto/info/customer/request/PatchNickNameDto.java
@@ -1,5 +1,6 @@
 package dto.info.customer.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Setter
 public class PatchNickNameDto {
 
+    @Schema(description = "변경하고자 하는 닉네임")
     private String nickName;
 
 }
--- domain-module/src/main/java/dto/info/customer/request/PatchOptionalTermDto.java
+++ domain-module/src/main/java/dto/info/customer/request/PatchOptionalTermDto.java
@@ -1,5 +1,6 @@
 package dto.info.customer.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Setter
 public class PatchOptionalTermDto {
 
+    @Schema(description = "변경하고자 하는 약관의 동의 여부", allowableValues = {"true", "false"})
     Boolean optionalTerm1;
 
 }
