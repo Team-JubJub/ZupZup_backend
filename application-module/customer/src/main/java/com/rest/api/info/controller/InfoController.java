e027335c0dc10fce33e7b1fc3d3a77a91fbf1621
김영후 <eagle7449@naver.com>
2023-08-05 18:21:40 +0900

    fix

--- application-module/customer/src/main/java/com/rest/api/info/controller/InfoController.java
+++ application-module/customer/src/main/java/com/rest/api/info/controller/InfoController.java
@@ -4,7 +4,6 @@ package com.rest.api.info.controller;
 import com.rest.api.auth.jwt.JwtTokenProvider;
 import com.rest.api.info.service.InfoService;
 import dto.auth.customer.request.PatchNickNameDto;
-import dto.auth.customer.response.DeleteUserDto;
 import dto.auth.customer.response.PatchNicknameResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
