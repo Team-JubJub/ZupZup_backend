4137bb0b01b3265338a0a051d5b3c17b14b6b883
김영후 <eagle7449@naver.com>
2023-09-09 21:04:29 +0900

    주문 목록 상품 imageUrl 추가

--- domain-module/src/main/java/domain/order/type/OrderSpecific.java
+++ domain-module/src/main/java/domain/order/type/OrderSpecific.java
@@ -13,6 +13,7 @@ public class OrderSpecific {
     //[상품 이름, 가격, 갯수, (이미지)]
     private Long itemId; // 일단 단순 Long으로 받아오고, 외래키로 갈지 말지는 나중에
     private String itemName;    // 상품 이름
+    private String imageUrl;    // 상품 이미지 url
     private Integer itemPrice;  // 가격
     @Min(value = 0, message = "상품의 개수는 0개 미만일 수 없습니다.")
     private Integer itemCount;  // 갯수
