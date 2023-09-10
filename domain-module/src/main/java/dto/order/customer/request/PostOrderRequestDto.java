a066eb212591844dc77ea2639e6385a7b05fa85d
김영후 <eagle7449@naver.com>
2023-09-10 15:26:34 +0900

    방문 시간 포맷 예시 작성

--- domain-module/src/main/java/dto/order/customer/request/PostOrderRequestDto.java
+++ domain-module/src/main/java/dto/order/customer/request/PostOrderRequestDto.java
@@ -1,6 +1,7 @@
 package dto.order.customer.request;
 
 import domain.order.type.OrderSpecific;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import java.util.List;
 @Setter
 public class PostOrderRequestDto {
 
+    @Schema(example = "yyyy-MM-dd HH:mm, 24h 포맷( ex) 2023-09-28 19:30 )")
     private String visitTime;
     private List<OrderSpecific> orderList; // 주문 품목 이름, 가격, 개수
 
