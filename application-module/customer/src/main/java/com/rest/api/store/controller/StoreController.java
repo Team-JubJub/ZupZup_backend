7d0ef174fe9837c6f0837c95161c095e757d65ca
김영후 <eagle7449@naver.com>
2023-09-23 16:01:38 +0900

    가게 조회 명세 리스트 형태로 변경

--- application-module/customer/src/main/java/com/rest/api/store/controller/StoreController.java
+++ application-module/customer/src/main/java/com/rest/api/store/controller/StoreController.java
@@ -46,13 +46,13 @@ public class StoreController {
     public ResponseEntity storeList(@Parameter(name = "accessToken", description = "액세스 토큰", in = ParameterIn.HEADER) @RequestHeader(JwtTokenProvider.ACCESS_TOKEN_NAME) String accessToken,
                                     @Parameter(name = "category", description = "조회할 카테고리(전체 조회할 시에는 쿼리 파트 없이 ~/store 로 요청)", in = ParameterIn.QUERY) @RequestParam(required = false) String category) {
         if(category != null) { // 카테고리 선택 시(우리는 카테고리 선택을 통해 조회하는 것이 메인 기능임)
-            List<GetStoreDto> allStoreDtoByCategoryList = storeService.storeListByCategory(category);
+            List<GetStoreDetailsDto> allStoreDtoByCategoryList = storeService.storeListByCategory(category);
             if (allStoreDtoByCategoryList.size() == 0) return new ResponseEntity(HttpStatus.NO_CONTENT);
 
             return new ResponseEntity(allStoreDtoByCategoryList, HttpStatus.OK);
         }
         else {  // 카테고리 선택안했을 시, 전체 가게 리턴
-            List<GetStoreDto> allStoreDtoList = storeService.storeList();
+            List<GetStoreDetailsDto> allStoreDtoList = storeService.storeList();
             if (allStoreDtoList.size() == 0) return new ResponseEntity(HttpStatus.NO_CONTENT);
 
             return new ResponseEntity(allStoreDtoList, HttpStatus.OK);
@@ -72,7 +72,7 @@ public class StoreController {
     })
     @GetMapping("/starred")
     public ResponseEntity starredStoreList(@Parameter(name = "accessToken", description = "액세스 토큰", in = ParameterIn.HEADER) @RequestHeader(JwtTokenProvider.ACCESS_TOKEN_NAME) String accessToken) {
-        List<GetStoreDto> allStoreDtoByStarredList = storeService.starredStoreList(accessToken);
+        List<GetStoreDetailsDto> allStoreDtoByStarredList = storeService.starredStoreList(accessToken);
         if (allStoreDtoByStarredList.size() == 0) return new ResponseEntity(HttpStatus.NO_CONTENT);
 
         return new ResponseEntity(allStoreDtoByStarredList, HttpStatus.OK);
--- domain-module/src/main/java/dto/store/customer/response/GetStoreDto.java
+++ domain-module/src/main/java/dto/store/customer/response/GetStoreDto.java
@@ -5,24 +5,27 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class GetStoreDto {
 
-    private Long storeId;
-
-    private String storeName;
-    private String storeImageUrl;
-    private String openTime;
-    private String closeTime;
-    private String saleTimeStart;
-    private String saleTimeEnd;
-
-    private Boolean isOpen;
-    private String closeDay;
-
-    private Integer starredUserCount;   // 찜한 사용자의 수
+//    private Long storeId;
+//
+//    private String storeName;
+//    private String storeImageUrl;
+//    private String openTime;
+//    private String closeTime;
+//    private String saleTimeStart;
+//    private String saleTimeEnd;
+//
+//    private Boolean isOpen;
+//    private String closeDay;
+//
+//    private Integer starredUserCount;   // 찜한 사용자의 수
+    List<GetStoreDetailsDto> stores;
 
 }
