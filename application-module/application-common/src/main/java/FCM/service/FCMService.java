27b9c14ee957c6d3d6fdbba889e630627e251a5b
김영후 <eagle7449@naver.com>
2023-10-03 16:03:32 +0900

    메세지 보내는 service 작성

--- application-module/application-common/src/main/java/FCM/service/FCMService.java
+++ application-module/application-common/src/main/java/FCM/service/FCMService.java
@@ -1,8 +1,14 @@
 package FCM.service;
 
+import FCM.dto.FCMAlertDto;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import repository.StoreRepository;
+import repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +16,22 @@ public class FCMService {
 
     private final FirebaseMessaging firebaseMessaging;
 
+    public String sendMessage(FCMAlertDto fcmAlertDto) {
+        Notification notification = Notification.builder()
+                .setTitle(fcmAlertDto.getTitle())
+                .setBody(fcmAlertDto.getBody())
+                .build();
+        Message message = Message.builder()
+                .setToken(fcmAlertDto.getTargetToken())
+                .setNotification(notification)
+                .build();
+        try {
+            firebaseMessaging.send(message);
+            return "알림을 성공적으로 보냈습니다.";
+        } catch (FirebaseMessagingException e) {
+            e.printStackTrace();
+            return "알림을 성공적으로 보내지 못했습니다.";
+        }
+    }
 
 }
