b908365e7be05fbbbd0c099808ac2e885f043421
김영후 <eagle7449@naver.com>
2023-10-19 23:02:16 +0900

    delete apple user dto 수정

--- domain-module/src/main/java/dto/auth/customer/request/DeleteAppleUserDto.java
+++ domain-module/src/main/java/dto/auth/customer/request/DeleteAppleUserDto.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class DeleteAppleUserDto {
 
-    @Schema(description = "애플 회원탈퇴에 필요한 authCode(apple 유저의 회원탈퇴가 아닐 시에는 빈 문자열을 요구)", allowableValues = {"", "authCode"})
-    String authCode;
+    @Schema(description = "애플 회원탈퇴에 필요한 refreshToken")
+    String refreshToken;
 
 }
