b562e339c72fc297e31d5d2ed46a400f114e91f2
김영후 <eagle7449@naver.com>
2023-12-07 16:35:48 +0900

    회원탈퇴 중인 사장님 451처리

--- application-module/application-common/src/main/java/exception/auth/seller/WantDeletionSellerException.java
+++ application-module/application-common/src/main/java/exception/auth/seller/WantDeletionSellerException.java
@@ -7,6 +7,6 @@ public class WantDeletionSellerException extends AuthRuntimeException {
 
     private static final String MESSAGE = "This seller is in deletion state";
 
-    public WantDeletionSellerException() { super(MESSAGE, HttpStatus.NOT_FOUND); }
+    public WantDeletionSellerException() { super(MESSAGE, HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS); }
 
 }
