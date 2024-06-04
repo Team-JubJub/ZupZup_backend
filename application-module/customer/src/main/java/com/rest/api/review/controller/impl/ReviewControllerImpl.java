9bb14aef58c956fe0d3c9feb922593225243690c
seasameoil <whdwlgus12@gmail.com>
2024-06-04 14:25:42 +0900

    추가 수정

--- application-module/customer/src/main/java/com/rest/api/review/controller/impl/ReviewControllerImpl.java
+++ application-module/customer/src/main/java/com/rest/api/review/controller/impl/ReviewControllerImpl.java
@@ -3,7 +3,7 @@ package com.rest.api.review.controller.impl;
 import com.rest.api.review.controller.ReviewController;
 import com.rest.api.review.model.dto.ReviewListResponse;
 import com.rest.api.review.model.dto.ReviewRequest;
-import com.rest.api.review.service.impl.ReviewServiceImpl;
+import com.rest.api.review.service.ReviewService;
 import com.zupzup.untact.social.jwt.SocialJwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,7 +18,7 @@ import java.util.List;
 @RequestMapping("/review")
 public class ReviewControllerImpl implements ReviewController {
 
-    private final ReviewServiceImpl reviewService;
+    private final ReviewService reviewService;
 
     @Override
     @PostMapping("")
@@ -34,19 +34,19 @@ public class ReviewControllerImpl implements ReviewController {
     @Override
     @GetMapping("")
     public ResponseEntity<List<ReviewListResponse>> findAll(@RequestHeader(SocialJwtTokenProvider.ACCESS_TOKEN_NAME) String accessToken,
-                                  @RequestParam(required = false, defaultValue = "0", value = "page") int pageNo) {
+                                  @RequestParam(required = false, defaultValue = "0", value = "page") int pageNo) throws Exception {
 
         List<ReviewListResponse> reviewList = reviewService.findAll(pageNo, accessToken);
-        return ResponseEntity.ok(reviewList);
+        return new ResponseEntity<>(reviewList, HttpStatus.OK);
     }
 
     @Override
     @DeleteMapping("/{reviewID}")
     public ResponseEntity<Long> delete(@RequestHeader(SocialJwtTokenProvider.ACCESS_TOKEN_NAME) String accessToken,
-                                 @PathVariable Long reviewID) {
+                                 @PathVariable Long reviewID) throws Exception {
 
-        Long deletedID = reviewService.delete(reviewID);
-        return ResponseEntity.ok(deletedID);
+        Long deletedID = reviewService.delete(reviewID, accessToken);
+        return new ResponseEntity<>(deletedID, HttpStatus.OK);
     }
 
 }
